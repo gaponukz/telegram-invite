b349f4c6f2d6fdb78e71284b7aedd79825a88a6f
lil_gappy_69 <49754258+gaponukz@users.noreply.github.com>
2020-09-26 10:08:03 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,45 @@
+from telethon.tl.functions.channels import InviteToChannelRequest
+from telethon.sync import TelegramClient
+from pyrogram import Client, filters
+import random, asyncio, json
+import sys, os
+
+with open('setting.json', 'r', encoding = 'utf') as out:
+    setting = json.load(out)
+
+sys.stdout = open(os.devnull, 'w')
+
+app = Client(
+    'my_account',
+    api_id = setting['account']['api_id'],
+    api_hash = setting['account']['api_hash']
+)
+
+client = TelegramClient(
+    setting['account']['username'], 
+    setting['account']['api_id'],
+    setting['account']['api_hash']
+)
+client.start()
+
+@app.on_message(filters.command('invite', prefixes = '/') & filters.me)
+async def invite_settings(_, message) -> None:
+    if not setting['channel_to_invite']:
+        channel = message.text.split('/invite ', maxsplit = 1)[1]
+    else:
+        channel = setting['channel_to_invite']
+    
+    members = await client.get_participants(message.chat.id)
+    delete_message = await message.delete()
+    chats = await client.get_dialogs()
+    dialog = [cht for cht in chats if channel in cht.name][0]
+
+    await client(
+        InviteToChannelRequest(
+            dialog, 
+            [mem.id for mem in members]
+        )
+    )
+
+if __name__ == "__main__":
+    app.run()
